2f202170b87c10a63d5d9302ea03f52ecad6a407
Brian Sanchez <brian36@fb.com>
2020-06-25 08:51:56 -0700

    Added PhotosViewController

---
+++ Tumblr/ViewController/PhotosViewController.m
@@ -0,0 +1,32 @@
+//
+//  PhotosViewController.m
+//  Tumblr
+//
+//  Created by Brian Sanchez on 6/25/20.
+//  Copyright © 2020 Brian Sanchez. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
